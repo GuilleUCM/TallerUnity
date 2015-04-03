293e0ae883bcf3e1848b96dee7b8b9ca400b98e2
Guille <guille.jimenez.diaz@gmail.com>
2015-04-03 11:39:56 +0200

    Se ha completado la lluvia de meteoros

---
+++ Assets/Scripts/DestroyMeteor.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyMeteor : MonoBehaviour {
+
+	public float minHeight = 0.0f;
+
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.y <= minHeight)
+			Destroy (gameObject);
+	}
+}
